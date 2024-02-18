13a52237db33971ef28eba46c2df7b574c6adbd4
Lawor55 <111243009+Lawor55@users.noreply.github.com>
2024-02-18 13:46:06 +0100

    Player Controlls

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //Variablen für Maus Input aus Unity
+    private float mouseX;
+    private float mouseY;
+
+    //Setting für Mausempfindlichkeit
+    [SerializeField]
+    private float sensitivity = 100f;
+
+    //Object für Player Object und Kamera
+    [SerializeField]
+    private Transform player;
+    private float camTilt = 0f;
+
+    //Gecalled wenn das Object fertig geladen hat
+    private void Start()
+    {
+        //lockt den cursor in der mitte des bildschirmes der anwendung und versteckt ihn
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //holt sich den Maus Input aus Unity, multipliziert es mit der eingestellten empfindlichkeit und deltaTime damit es unabhängig von der Framerate ist
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        //begrenzt camTilt auf einen 180° bereich damit man maximal gerade nach oben und unten schauen kann
+        camTilt -= mouseY;
+        camTilt = Mathf.Clamp(camTilt, -90f, 90f);
+
+        //wendet die X Rotation auf die Z achse des Spielers an um das gesammte Model zu drehen und die Y Rotation auf die Kamera
+        transform.localRotation = Quaternion.Euler(camTilt, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Settings")]
+    //movementspeed in meters per second
+    [SerializeField] private float speed = 5f;
+    //jump height in meters
+    [SerializeField] private float jumpHeight = 2f;
+    private float groundTime;
+
+    [Header("Physics Settings")]
+    //gravity acceleration
+    [SerializeField] private float gravity = -9.81f;
+    //variable for jumping velocity
+    private float verticalVelocity;
+
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+    private void Move()
+    {
+        //variation of official Unity documentation of CharacterController
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+
+        //applying of gravity to the verticalVelocity        
+        if (controller.isGrounded == false)
+        {
+            verticalVelocity = controller.velocity.y;
+        }
+        verticalVelocity += gravity * Time.deltaTime;
+        //exchange the vertical coordinate y with the calculated vertical velocity
+        move *= speed;
+        move.y = verticalVelocity;
+        //applies the movement of the character
+        controller.Move(Time.deltaTime * move);
+    }
+
+    private void Jump()
+    {
+        //groundcheck might need to be extended to allow jumps while walking down stairs
+        if (groundTime > 0 && Input.GetKey(KeyCode.Space))
+        {
+            groundTime = 0;
+            //Debug.Log("Jump!");
+            //formula to calculate the jump acceleration based on gravity and the desired jump height
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+    private void Update()
+    {
+        Jump();
+        //sets ground time to 0.2 seconds to extend ground check times while walking down stairs
+        if (controller.isGrounded)
+        {
+            groundTime = 0.2f;
+        }
+
+        if (groundTime > 0)
+        {
+            groundTime -= Time.deltaTime;
+        }
+        //resets the vertical velocity to 0 if the player touches ground and velocity is negative
+        if (controller.isGrounded && verticalVelocity < 0f)
+        {
+            //leads to stairlike walking when walking downwards
+            //verticalVelocity = 0f;
+
+            //leads to ramp like walking when walking downwards
+            verticalVelocity = -2f;
+        }
+        Move();
+    }
+}
